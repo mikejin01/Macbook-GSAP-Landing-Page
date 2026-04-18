cfd15985eb21ced53ee8ae99da3b9a8eaa61b402
Mike Jin <mike@xocontinental.com>
2026-04-18 13:20:34 -0400

    implement footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,28 @@
+import { footerLinks } from "@/constants"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <div className="info">
+            <p>
+                More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.
+            </p>
+            <img src="/logo.svg" alt="Apple logo" />
+        </div>
+        <hr />
+        <div className="links">
+            <p>
+                Copyright © 2026 Apple Inc. All rights reserved.
+            </p>
+            <ul>
+                {footerLinks.map(({ label, link }) => (
+                    <li key={label}>
+                        <a href={link}>{label}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </footer>
   )
 }
 
--- src/components/Performance.tsx
+++ src/components/Performance.tsx
@@ -69,7 +69,7 @@ const Performance = () => {
         <section id="performance" ref={sectionRef}>
             <h2>Next-level graphics performance. Game on.</h2>
 
-             <div className="wrapper">
+            <div className="wrapper">
                 {performanceImages.map((item, index) => (
                     <img
                         key={index}
@@ -78,7 +78,7 @@ const Performance = () => {
                         alt={item.alt || `Performance Image #${index + 1}`}
                     />
                 ))}
-             </div>
+            </div>
 
             <div className="content">
                 <p>
